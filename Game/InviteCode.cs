44d27c187cd6ab7f605d53e71721f3945c291f64
Oleksii Chernykh <7448849+uspiritwolf@users.noreply.github.com>
2024-02-14 00:13:46 +0200

    Added invite code thta help to find the lobby id

---
+++ Game/InviteCode.cs
@@ -0,0 +1,44 @@
+﻿using DobirnaGraServer.Utils;
+
+namespace DobirnaGraServer.Game
+{
+	public sealed class InviteCode : IDisposable
+	{
+		public string Code { get; init; }
+
+		public InviteCode(Guid id)
+		{
+			Code = GenerateUniqueToken();
+
+			AssociatedCodes.Add(Code, id);
+		}
+
+		~InviteCode()
+		{
+			Dispose();
+		}
+
+		public void Dispose()
+		{
+			AssociatedCodes.Remove(Code);
+		}
+
+		private static readonly IDictionary<string, Guid> AssociatedCodes = new Dictionary<string, Guid>();
+
+		private static string GenerateUniqueToken()
+		{
+			string token;
+
+			do
+			{
+				token = Secrets.GenerateTokenUrlSafe(6);
+			} while (AssociatedCodes.ContainsKey(token));
+
+			return token;
+		}
+
+		public static bool FindId(string inviteCode, out Guid id) => AssociatedCodes.TryGetValue(inviteCode, out id);
+
+		public static implicit operator string(InviteCode invite) => invite.Code;
+	}
+}
---
+++ Utils/Secrets.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+
+namespace DobirnaGraServer.Utils
+{
+	public static class Secrets
+	{
+		public static string GenerateTokenUrlSafe(int length)
+		{
+			using var rng = RandomNumberGenerator.Create();
+			byte[] bytes = new byte[length];
+			rng.GetBytes(bytes);
+			return Convert.ToBase64String(bytes)
+				.TrimEnd('=')
+				.Replace('+', '-')
+				.Replace('/', '_');
+		}
+	}
+}

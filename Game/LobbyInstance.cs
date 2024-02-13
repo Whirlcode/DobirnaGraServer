9ac2d9f29c5a183db60c5f3cdb89955f69f18f03
Oleksii Chernykh <7448849+uspiritwolf@users.noreply.github.com>
2024-02-14 00:15:45 +0200

    Added a basic lobby implementation where the state of the lobby game will be stored

---
+++ Game/LobbyInstance.cs
@@ -0,0 +1,46 @@
+﻿namespace DobirnaGraServer.Game
+{
+	public sealed class LobbyInstance : IDisposable
+	{
+		public Guid Id { get; init; }
+
+		public InviteCode InviteCode { get; init; }
+
+		public UserList Users { get; init; }
+
+		public LobbyInstance()
+		{
+			Id = Guid.NewGuid();
+			InviteCode = new InviteCode(Id);
+			Users = new UserList();
+		}
+
+		~LobbyInstance()
+		{
+			Dispose();
+		}
+
+		public void Dispose()
+		{
+			Users.Clear();
+			InviteCode.Dispose();
+		}
+
+		public bool HasUser(UserInstance user)
+		{
+			return Users.Any(e => e == user);
+		}
+
+		public void JoinUser(UserInstance user)
+		{
+			Users.AddUser(user);
+			user.CurrentLobby = this;
+		}
+
+		public void LeaveUser(UserInstance user)
+		{
+			Users.RemoveUser(user);
+			user.CurrentLobby = null;
+		}
+	}
+}
--- Game/UserInstance.cs
+++ Game/UserInstance.cs
@@ -10,6 +10,19 @@ namespace DobirnaGraServer.Game
 
 		public bool IsValid => Context != null;
 
+		public LobbyInstance? CurrentLobby
+		{
+			get => (LobbyInstance?)Context?.Items[nameof(CurrentLobby)];
+			set
+			{
+				if (Context == null)
+					throw new InvalidOperationException("User is dead!");
+				if (value != null && !value.HasUser(this))
+					throw new InvalidOperationException("The user is not in the group.");
+				Context.Items[nameof(CurrentLobby)] = value;
+			}
+		}
+
 		public void Abandon()
 		{
 			WeakContext.Target = null;

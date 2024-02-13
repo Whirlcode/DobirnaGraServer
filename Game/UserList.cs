89e62a57aca1ba24418fab8be5fd7dc5bcf073b2
Oleksii Chernykh <7448849+uspiritwolf@users.noreply.github.com>
2024-02-14 00:14:32 +0200

    Added specific list that stores User Instance by weak references

---
+++ Game/UserList.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace DobirnaGraServer.Game
+{
+	public class UserList : IEnumerable<UserInstance>
+	{
+		private List<WeakReference> _users = new();
+
+		public void AddUser(UserInstance user)
+		{
+			_users.Add(new WeakReference(user));
+			Validate();
+		}
+
+		public void RemoveUser(UserInstance user)
+		{
+			_users.RemoveAll(u => u.Target == user);
+			Validate();
+		}
+
+		public void Clear()
+		{
+			_users.Clear();
+		}
+
+		private void Validate()
+		{
+			_users.RemoveAll(u => u.IsAlive == false);
+		}
+
+		public IEnumerator<UserInstance> GetEnumerator()
+		{
+			foreach(var weak in _users)
+			{
+				if (weak is { IsAlive: true, Target: UserInstance user })
+				{
+					yield return user;
+				}
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}

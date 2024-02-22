36b733c0c0892ac9db782ee28f79b59744d4c63e
Oleksii Chernykh <7448849+uspiritwolf@users.noreply.github.com>
2024-02-22 18:55:16 +0200

    Added some extensions to simplify code

---
+++ Utils/IntExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace DobirnaGraServer.Utils
+{
+	public static class IntExtension
+	{
+		private enum Kinds
+		{
+			Bytes = 0,
+			Kilobytes = 1,
+			Megabytes = 2,
+			Gigabytes = 3,
+			Terabytes = 4,
+		}
+
+		public static int Megabytes(this int value)
+		{
+			return (int)(value * Math.Pow(1024, (int)Kinds.Megabytes));
+		}
+	}
+}

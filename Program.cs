c2e4dd3eb1c1841ef29e1371d12b6c61ca3f9231
Oleksii Chernykh <7448849+uspiritwolf@users.noreply.github.com>
2024-02-22 02:32:18 +0200

    Allowed CORS to send resource for client

--- Program.cs
+++ Program.cs
@@ -14,12 +14,23 @@ namespace DobirnaGraServer
 			builder.Services.AddControllers();
 			builder.Services.AddSignalR().AddMessagePackProtocol();
 
+			builder.Services.AddCors(options =>
+			{
+				options.AddDefaultPolicy(policy =>
+				{
+					policy.AllowAnyOrigin();
+					policy.AllowAnyHeader();
+					policy.AllowAnyMethod();
+				});
+			});
+
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
 
 			builder.Services.AddSingleton<ProfileService>();
 			builder.Services.AddSingleton<GameService>();
+			builder.Services.AddSingleton<ResourceService>();
 
 			var app = builder.Build();
 
@@ -32,9 +43,11 @@ namespace DobirnaGraServer
 
 			app.UseHttpsRedirection();
 
+			app.UseCors();
+
 			app.UseAuthorization();
 
-			app.MapControllers();
+			app.MapControllers().RequireCors();
 			app.MapHub<GameHub>("/gamehub");
 
 			app.Run();

17946d05158c7a7533746ea5ea55f166873b2095
Oleksii Chernykh <7448849+uspiritwolf@users.noreply.github.com>
2024-03-11 22:55:08 +0200

    Added struct of the round game state

--- Models/GameRPC/GameStateMessage.cs
+++ Models/GameRPC/GameStateMessage.cs
@@ -12,6 +12,7 @@ namespace DobirnaGraServer.Models.GameRPC
 	[JsonPolymorphic(TypeDiscriminatorPropertyName = "Type")]
 	[JsonDerivedType(typeof(BaseStateData), typeDiscriminator: "base")]
 	[JsonDerivedType(typeof(IdleStateData), typeDiscriminator: "idle")]
+	[JsonDerivedType(typeof(IdleStateData), typeDiscriminator: "round")]
 	public class BaseStateData(string type)
 	{
 		public string Type { get; init; } = type; // hack for .NET 8, because SignalR not support JsonPolymorphic
@@ -20,5 +21,13 @@ namespace DobirnaGraServer.Models.GameRPC
 	public class IdleStateData() : BaseStateData("idle")
 	{
 		public required List<Guid> ReadyUsers { get; init; }
+	}
+
+
+	public class RoundStateData() : BaseStateData("round")
+	{
+		public required Dictionary<string, List<int>> Questions;
+
+		public required Guid? Electioneer;
 	}
 }
